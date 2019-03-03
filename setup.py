04fa49e6e4f573da55d92e2dcdbc4df6a2cd1412
pb2921 <priyanshu2921@gmail.com>
2019-03-03 19:09:19 +1100

    Including txt files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='randomNameGen',  
+     name='randomNameGenerator',  
      version='0.1',
      scripts=['randomName.py'] ,
      author="Priyanshu Bhatt",
@@ -12,7 +12,12 @@ setuptools.setup(
    long_description_content_type="text/markdown",
      url="https://github.com/pb2921/randomNameGen",
      packages=setuptools.find_packages(),
-     classifiers=[
+	include_package_data=True,
+	package_dir={'randomNameGen':'randomNameGen'}, # the one line where all the magic happens
+     package_data={
+      'randomNameGen': ['1000Names.txt','100lastNames.txt'],
+     },
+	classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
